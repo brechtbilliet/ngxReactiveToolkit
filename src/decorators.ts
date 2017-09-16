d5796bf772bedfb0e3b05ac527cec1ea5c5fdf9e
Brecht Billiet <billietbrecht@gmail.com>
2017-09-16 09:41:57 +0200

    ability to pass key to change decorator

--- src/decorators.ts
+++ src/decorators.ts
@@ -2,6 +2,8 @@ import 'rxjs/add/observable/interval';
 import 'rxjs/add/operator/takeUntil';
 import { SimpleChanges } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/filter';
 
 // These decorators are all about utils to turn lifecycle events into streams
 /*
@@ -50,15 +52,22 @@ export function Destroy() {
         }
     }
 */
-export function Changes() {
+export function Changes(inputProp?: string) {
     return function (target: any, key: string) {
-        target[key] = new Subject();
+        const subject = new Subject();
+        target[key] = inputProp ? subject
+            .filter(changes => !!changes && changes[inputProp] && changes[inputProp].currentValue)
+            .map(changes => changes[inputProp].currentValue) : subject;
         const oldNgOnChanges = target.ngOnChanges;
+        if (!target._subjectsToNext) {
+            target._subjectsToNext = [];
+        }
+        target._subjectsToNext.push(subject);
         target.ngOnChanges = (simpleChanges: SimpleChanges) => {
             if (oldNgOnChanges) {
                 oldNgOnChanges();
             }
-            target[key].next(simpleChanges);
+            target._subjectsToNext.forEach(sub => sub.next(simpleChanges));
         }
     }
 }
