a88d95c2a7682176d0d2de6bcbc0efdc7af0298c
Ruben Vermeulen <hello@rubenvermeulen.be>
2018-12-05 21:03:29 +0100

    updates module and jest processor to not use babel-core anymore

--- jest-preprocessor.js
+++ jest-preprocessor.js
@@ -3,23 +3,10 @@ const tsConfig = require('./tsconfig.jest.json');
 
 module.exports = {
   process(src, path) {
-    if (path.endsWith('.ts')) {
-      // when we are dealing with typescript files
-      // it's important to compile it to javascript first
-      // and afterwards compile it through babel
-      const compiled = tsc.transpile(src, tsConfig.compilerOptions, path, []);
-      return transpileJs(compiled);
+    if (path.endsWith('.ts') || path.endsWith('.js')) {
+      return tsc.transpile(src, tsConfig.compilerOptions, path, []);
     }
-    if (path.endsWith('.js')) {
-      return transpileJs(src);
-    }
+
     return src;
   }
 };
-
-function transpileJs(src) {
-  return require('babel-core').transform(src, {
-    sourceMap: true,
-    presets: ['es2015', 'stage-0']
-  });
-}
